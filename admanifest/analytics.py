347b6e4b055a10b0dce515059cb7d08ffd87fcde
Mantas <sirexas@gmail.com>
2018-10-14 17:56:18 +0300

    Some work on progress

--- admanifest/analytics.py
+++ admanifest/analytics.py
@@ -1,2 +1,5 @@
 def get_timeline_by_stars(manifest):
-    return []
+    for source in manifest.objects['source'].values():
+        for object in source.get('objects', {}).values():
+            for field in object.get('properties', {}).values():
+                yield source['id'], object['id'], field['stars']
